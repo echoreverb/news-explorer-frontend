af3398fec93d778b8b2adeb19544a1e2cd63eebe
Alexey Dikarev <mailsub@yandex.ru>
2020-09-09 18:34:02 +0300

    fix: update newsApi baseUrl

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -2,7 +2,7 @@ export const MAIN_PATH = '/news-explorer-frontend/';
 export const ARTICLES_PATH = '/news-explorer-frontend/articles.html';
 
 export const NEWS_API_OPTIONS = {
-  baseUrl: 'https://nomoreparties.co/v2/everything?q=',
+  baseUrl: 'https://nomoreparties.co/news/v2/everything?q=',
   key: 'f2b033a2d3804b5aaf33852777f3c31e',
   newsLang: 'ru',
   searchPeriodDays: 7,
